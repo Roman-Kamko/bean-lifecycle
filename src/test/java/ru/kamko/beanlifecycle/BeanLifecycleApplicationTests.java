3193564a9877cdff58afd0c58595508302e18f95
Roman-Kamko <r.kamko@mail.ru>
2024-02-15 15:40:15 +0400

    initial commit

---
+++ src/test/java/ru/kamko/beanlifecycle/BeanLifecycleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kamko.beanlifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanLifecycleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
